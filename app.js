4bbe9b2dc77fad32cd477d1718acd190bf40cc42
Mojo Codess <jcakelley@gmail.com>
2017-11-12 23:23:28 -0600

    commented code for delete button

---
+++ app.js
@@ -0,0 +1,44 @@
+function onReady(){
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+  const deleteToDo = document.getElementById('deleteToDo');
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    let title = newToDoText.value;
+    let newLi = document.createElement('li');
+    let checkbox = document.createElement('input');
+
+
+    checkbox.type = "checkbox";
+    newLi.textContent = title;
+    newLi.appendChild(checkbox);
+    toDoList.appendChild(newLi);
+    newToDoText.value = '';
+
+  });
+
+  deleteToDo.addEventListener('click', removal => {
+  removal.preventDefault();
+  let selected = document.querySelectorAll('#toDoList li input:checked');
+    for(var i = 0; i < selected.length; i++){
+      // input:checked's parent's parent aka the grandparent; requesting grandparent to
+      // removeChild(input:checked's parent); selected[i] identifies a specfic element in the collection
+      // of checkboxes
+     selected[i].parentNode.parentNode.removeChild(selected[i].parentNode);
+    }
+  });
+}
+  // document.getElementById('deleteToDo').addEventListener('click'){
+  //   checkbox.checked();
+  // });
+
+
+document.querySelectorAll('#items-listed li input:checked')
+window.onload = function(){
+  alert("The window has loaded!");
+  onReady();
+  // deleteToDoItem();
+}

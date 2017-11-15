395d385a3e4aa2b88d3ecb0996cca979a8717bb5
Mojo Codess <jcakelley@gmail.com>
2017-11-15 14:36:32 -0600

    Completes checkpoint 8

--- app.js
+++ app.js
@@ -1,47 +1,69 @@
 function onReady(){
-  const addToDoForm = document.getElementById('addToDoForm');
+const toDos = [];
+const addToDoForm = document.getElementById('addToDoForm');
+let idS = 0;
+
+function creatNewToDo() {
   const newToDoText = document.getElementById('newToDoText');
+  if (!newToDoText.value){return;}
+
+  toDos.push({
+    title: newToDoText.value,
+    complete: false,
+    id: idS
+  });
+  // console.log(toDos[0]);
+  // console.log(toDos[1]);
+// id is possibly not incrememnting...why? is it the object? Nope!!! id is incrememnting as it should
+  idS++;
+
+newToDoText.value = '';
+renderTheUI();
+}
+
+function renderTheUI() {
   const toDoList = document.getElementById('toDoList');
-  const deleteToDo = document.getElementById('deleteToDo');
+
+  toDoList.textContent = '';
+
+  toDos.forEach(function(toDo){
+    const newLi = document.createElement('li');
+    const checkbox = document.createElement('input');
+    const deleteToDo = document.createElement('button');
+
+    checkbox.type = "checkbox";
+
+    newLi.textContent = toDo.title;
+    deleteToDo.innerText = 'Delete';
+
+    toDoList.appendChild(newLi);
+    newLi.appendChild(checkbox);
+    newLi.appendChild(deleteToDo);
+
+
+     deleteToDo.addEventListener('click', removal =>{
+      let removalList = toDos.filter(list => toDo.id == idS);
+        toDos.splice(toDo.id);
+        renderTheUI();
+      });
+
+  });
+
+
+
+}
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
-
-    let title = newToDoText.value;
-    let newLi = document.createElement('li');
-    let checkbox = document.createElement('input');
-
-
-    checkbox.type = "checkbox";
-    newLi.textContent = title;
-    newLi.appendChild(checkbox);
-    toDoList.appendChild(newLi);
-    newToDoText.value = '';
-
+    creatNewToDo();
   });
 
-  deleteToDo.addEventListener('click', removal => {
-  removal.preventDefault();
-  let selected = document.querySelectorAll('#toDoList li input:checked');
-    for(var i = 0; i < selected.length; i++){
-<<<<<<< HEAD
-      // input:checked's parent's parent aka the grandparent; requesting grandparent to
-      // removeChild(input:checked's parent); selected[i] identifies a specfic element in the collection
-      // of checkboxes
-=======
->>>>>>> 68c2ac58d8d1646267e35c9ff06d78118bcb020d
-     selected[i].parentNode.parentNode.removeChild(selected[i].parentNode);
-    }
-  });
+renderTheUI();
 }
-  // document.getElementById('deleteToDo').addEventListener('click'){
-  //   checkbox.checked();
-  // });
 
 
-document.querySelectorAll('#items-listed li input:checked')
+
 window.onload = function(){
-  alert("The window has loaded!");
   onReady();
-  // deleteToDoItem();
-}
+
+};
